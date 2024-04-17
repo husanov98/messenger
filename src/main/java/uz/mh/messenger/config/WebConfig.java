7286355205b4deab91b1b37bcc859b1feba1c0f0
husanov98 <muhammadhusanov98@gmail.com>
2024-04-17 10:15:33 +0500

    all are allowed

--- src/main/java/uz/mh/messenger/config/WebConfig.java
+++ src/main/java/uz/mh/messenger/config/WebConfig.java
@@ -8,7 +8,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
